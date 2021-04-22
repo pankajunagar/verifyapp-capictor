6118e2e9df63c6ec35a226da5b4eb580422da2de
Harsh vidyarthi <harshvidyarthi3359@gmail.com>
2021-04-22 19:36:16 +0530

    initial commit

--- src/app/conatants/MainAppSetting.ts
+++ src/app/conatants/MainAppSetting.ts
@@ -116,11 +116,11 @@ export class MainAppSetting {
             this.storageService.storeDataToIonicStorage('appSrc', 'rentals');
 
             if (this.appFor == 'alpha') {
-                //   API = 'https://www.nowverifyit.com';
-                API = 'http://develop.nowverifyit.com'; //charu
+                  API = 'https://www.nowverifyit.com';
+                // API = 'http://develop.nowverifyit.com'; //charu
             } else if (this.appFor == 'production') {
-                // API = 'https://www.nowverifyit.com';
-                API = 'http://develop.nowverifyit.com';//charu
+                API = 'https://www.nowverifyit.com';
+                // API = 'http://develop.nowverifyit.com';//charu
             }
         } else if (this.ORG == "BM") {
             window.localStorage.setItem('appSrc', 'building-management');
