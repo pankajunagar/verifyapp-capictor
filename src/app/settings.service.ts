987915f4f3f352805b81cb3fe4b44f74337bce4f
Navjot <navjotsingh1795@gmail.com>
2021-05-01 15:37:22 +0530

    pwa add to home screen

---
+++ src/app/settings.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+
+  private theme: BehaviorSubject<String>;
+
+  constructor() { 
+
+    this.theme = new BehaviorSubject('red-theme');
+  }
+
+  setActiveTheme(val){
+    this.theme.next(val);
+  }
+
+getActiveTheme(){
+    return this.theme.asObservable();
+}
+
+}
